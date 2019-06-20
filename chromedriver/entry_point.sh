cf5d2e3b1d69c8f15bd9a7a4de8a0d74d21b2546
Mohamed <mhamzawey@aucegypt.edu>
2019-06-21 01:53:54 +0200

    clone recursivly check in test

--- chromedriver/entry_point.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-ln -s /etc/nginx/sites-available/check-api /etc/nginx/sites-enabled/check-api
-echo 'Starting Nginx...'
-nginx &
-echo 'Starting regular Chromedriver entry point...'
-/opt/bin/entry_point.sh
