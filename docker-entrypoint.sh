e91cf702a7153273ea138d3343c2a7513e10b506
hfikry92 <hfikry92@gmail.com>
2019-12-10 12:33:51 +1100

    cloning configurator at run time

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+if [[ -z ${GITHUB_TOKEN+x} || -z ${DEPLOY_ENV+x} || -z ${APP+x}  ]]; then
+	echo "GITHUB_TOKEN, DEPLOY_ENV, and APP must be in the environment. Exiting."
+	exit 1
+fi
+if [ ! -d "configurator" ]; then git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/meedan/configurator ./configurator; fi
+d=configurator/check/${DEPLOY_ENV}/${APP}/; for f in $(find $d -type f); do cp "$f" "${f/$d/}"; done
+
+
 npm run publish
